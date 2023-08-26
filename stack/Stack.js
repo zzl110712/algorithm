209db50f316aaf21041e2768701dac5b8e3f5d50
Zhao <m13840389047@163.com>
2023-08-26 19:46:38 +0800

    栈

---
+++ stack/Stack.js
@@ -0,0 +1,10 @@
+/**
+ * 栈是一个后进先出的数据结构
+ * JavaScript中可用Array模拟栈的功能
+ */
+
+const stack = [] // 栈
+stack.push(1) // 入栈
+stack.push(2) // 入栈
+let item1 = stack.pop() // 出栈 pop:移除数组最后一项并返回这个元素
+let item2 = stack.pop() // 出栈 pop:移除数组最后一项并返回这个元素
